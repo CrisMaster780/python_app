bcf7a9c7e4bb78142be7bd10c4e7a8356cb47e78
CrisMaster780 <95488614+CrisMaster780@users.noreply.github.com>
2024-02-13 15:53:55 -0300

    commit

--- Clientes/models.py
+++ Clientes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Clientes(models.Model):
     nombre =  models.CharField(max_length = 250)
     apellido = models.CharField(max_length = 250)
-    documento = models.CharField(max_length = 250)
+    documento = models.CharField(max_length = 250, unique=True)
     direccion =  models.CharField(max_length = 250)
     telefono = models.CharField(max_length = 250)
     correo =  models.CharField(max_length = 250)
